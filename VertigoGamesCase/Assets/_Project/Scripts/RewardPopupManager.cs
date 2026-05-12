0baadbe5bfdeac1107ebbaac786f20131bb39e58
Beyzanur Mollaoğlu <byzmllogluu@gmail.com>
2026-05-12 20:44:32 +0300

    Update RewardPopupManager.cs

--- VertigoGamesCase/Assets/_Project/Scripts/RewardPopupManager.cs
+++ VertigoGamesCase/Assets/_Project/Scripts/RewardPopupManager.cs
@@ -57,7 +57,6 @@ public class RewardPopupManager : MonoBehaviour
     {
         if (_lastEarnedSprite == null) return;
 
-        // 1. Önce objeyi oluştur ve ayarlarını yap
         GameObject spawnedItem = Instantiate(rewardItemPrefab, transform.parent);
         CollectedItem itemScript = spawnedItem.GetComponent<CollectedItem>();
 
@@ -71,10 +70,8 @@ public class RewardPopupManager : MonoBehaviour
         RectTransform itemRect = spawnedItem.GetComponent<RectTransform>();
         itemRect.position = rewardIcon.transform.position;
 
-        // Paneli hemen kapat
         popupPanel.SetActive(false);
 
-        // 2. Animasyon dizisi (Sequence) oluştur
         Sequence collectSequence = DOTween.Sequence();
 
         collectSequence.Append(itemRect.DOMove(leftPanelContent.position, 0.8f).SetEase(Ease.InQuart));
@@ -82,14 +79,11 @@ public class RewardPopupManager : MonoBehaviour
 
         collectSequence.OnComplete(() =>
         {
-            // false parametresi objenin panelin pivot/scale ayarlarına hemen uymasını sağlar
             spawnedItem.transform.SetParent(leftPanelContent, false);
 
-            // Scale ve Rotation sıfırlama
             spawnedItem.transform.localScale = Vector3.one;
             spawnedItem.transform.localRotation = Quaternion.identity;
 
-            // Layout'u anında hesaplaması için zorla
             Canvas.ForceUpdateCanvases();
             LayoutRebuilder.ForceRebuildLayoutImmediate(leftPanelContent.GetComponent<RectTransform>());
         });
